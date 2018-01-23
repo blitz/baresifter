e56d992c5885a836b2b71b5d6b9dba4daf46bdf2
Julian Stecklina <js@alien8.de>
2018-01-24 00:58:53 +0100

    Execute constructors on startup

--- main.cpp
+++ main.cpp
@@ -3,8 +3,12 @@
 #include "io.hpp"
 
 extern "C" void start();
+extern "C" void (*_init_array_start[])();
+extern "C" void (*_init_array_end[])();
 
 void start()
 {
-  print_string("Hello World!");
+  print_string(">>> Executing constructors.\n");
+  for (auto p = _init_array_start; p < _init_array_end; p++)
+    (*p)();
 }
