8e8cda0e9bcdd57f9d870b0693d9ebbb2a5750d3
Julian Stecklina <js@alien8.de>
2018-02-05 22:52:01 +0100

    Fix non-LTO build

--- stdlib.cpp
+++ stdlib.cpp
@@ -1,8 +1,16 @@
 #include <string.h>
 
+#include "util.hpp"
+
 // We have to mark these functions as used, because calls to them are generated
 // by the compiler.
 #define used __attribute__((used))
+
+extern "C" void __cxa_pure_virtual()
+{
+  format("Abstract method called from ", hex((uintptr_t)__builtin_return_address(0)), "\n");
+  wait_forever();
+}
 
 used void *memset(void *s, int c, size_t n)
 {
@@ -23,4 +31,11 @@ size_t strlen(const char *s)
   size_t i = 0;
   for (; s[i] != 0; i++);
   return i;
+}
+
+int strcmp(const char *s1, const char *s2)
+{
+  int r = 0;
+  while ((r = *(s1++) - *(s2++)) == 0 and s1[-1]);
+  return r;
 }
