44b18aa121d8f73c5750f04a5f483c388a6322bf
Julian Stecklina <js@alien8.de>
2019-01-15 00:12:26 +0100

    Fixed run.sh to work without display
    ... otherwise qemu would fail to start because of:
    
    Unable to init server: Could not connect: Connection refused
    gtk initialization failed

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-set -e
+set -e -u
 
 scons -s -j$(nproc) baresifter.elf32
-exec qemu-system-x86_64 -enable-kvm -cpu host -no-reboot -vga none -debugcon stdio -kernel baresifter.elf32
+exec qemu-system-x86_64 \
+     -enable-kvm -cpu host \
+     -no-reboot \
+     -display none -vga none -debugcon stdio \
+     -kernel baresifter.elf32
