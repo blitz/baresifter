c6a8c3199254699eaec708b279035fceb170cde9
Julian Stecklina <js@alien8.de>
2019-02-08 00:04:46 +0100

    Update capstone to 4.0.1

--- include/limits.h
+++ include/limits.h
@@ -2,4 +2,17 @@
 
 #define UCHAR_MAX 255
 
+#if __SIZEOF_INT__ == 4
+# define INT_MAX  2147483647
+#endif
+
+#if __SIZEOF_LONG__ == 4
+# define LONG_MAX 2147483647L
+#elif __SIZEOF_LONG__ == 8
+# define LONG_MAX 9223372036854775807L
+#endif
+
+#define INT_MIN (-INT_MAX - 1)
+#define LONG_MIN (-LONG_MAX - 1L)
+
 #define weak_alias(a, b)
