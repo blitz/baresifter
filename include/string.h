2e3912a69eaf59a84c7b86a37be057f4f997e57c
Julian Stecklina <js@alien8.de>
2019-02-09 00:57:38 +0100

    Ingest strtok() from musl

--- include/string.h
+++ include/string.h
@@ -10,3 +10,7 @@ EXTERN_C char *strncpy(char *dest, const char *src, size_t n);
 EXTERN_C size_t strlen(const char *s);
 EXTERN_C int strcmp(const char *s1, const char *s2);
 EXTERN_C int strncmp(const char *s1, const char *s2, size_t n);
+EXTERN_C char *__strchrnul(const char *s, int c);
+EXTERN_C size_t strcspn(const char *s, const char *c);
+EXTERN_C size_t strspn(const char *s, const char *c);
+EXTERN_C char *strtok(char *s, const char *sep);
