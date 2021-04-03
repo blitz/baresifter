ff6dd9c28120b9fd159977f1335aeeade5d03346
Julian Stecklina <js@alien8.de>
2021-04-03 19:41:28 +0200

    Run 32-bit mode in CI

--- nix/release.nix
+++ nix/release.nix
@@ -2,12 +2,13 @@
 , nixpkgs ? sources.nixpkgs
 , pkgs ? import nixpkgs { }
 }:
-rec {
+let
   baresifter = pkgs.callPackage ./build.nix { };
 
-  baresifter-run = pkgs.runCommandNoCC "baresifter-run" {
-    nativeBuildInputs = [ pkgs.makeWrapper ];
-  } ''
+  baresifter-run = pkgs.runCommandNoCC "baresifter-run"
+    {
+      nativeBuildInputs = [ pkgs.makeWrapper ];
+    } ''
     mkdir -p $out/bin
 
     install -m 0755 ${../run.sh} $out/bin/baresifter-run
@@ -17,14 +18,20 @@ rec {
       --prefix PATH : ${pkgs.lib.makeBinPath (with pkgs; [ qemu file binutils-unwrapped ])}
   '';
 
-  test-qemu-tcg = pkgs.runCommandNoCC "test-qemu-tcg" {
-    nativeBuildInputs = [ baresifter-run ];
-  } ''
-    timeout 120 baresifter-run tcg ${baresifter}/share/baresifter/baresifter.x86_64.elf \
-      tcg stop_after=100 | tee out.log
+  testcase = { mode, binary }: pkgs.runCommandNoCC "test-qemu-tcg"
+    {
+      nativeBuildInputs = [ baresifter-run ];
+    } ''
+    timeout 120 baresifter-run ${mode} ${baresifter}/share/baresifter/${binary} \
+      stop_after=100 | tee out.log
 
     grep -Fq ">>> Done" out.log || echo "Test did not complete successfully."
     cp out.log $out
   '';
+in
+{
+  inherit baresifter baresifter-run;
 
+  test-x86_64-tcg = testcase { mode = "tcg"; binary = "baresifter.x86_64.elf"; };
+  test-x86_32-tcg = testcase { mode = "tcg"; binary = "baresifter.x86_32.elf"; };
 }
