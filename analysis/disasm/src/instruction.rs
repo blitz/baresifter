a0cf7624970f6f933965075127c686f35c48ac56
Julian Stecklina <js@alien8.de>
2021-04-13 00:40:53 +0200

    Implement an iterator that interpolates between baresifter lines

--- analysis/disasm/src/instruction.rs
+++ analysis/disasm/src/instruction.rs
@@ -9,7 +9,7 @@ use std::fmt;
 ///
 /// The internal representation is geared for small size and lack of
 /// heap allocations.
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Instruction {
     /// This is actually two fields: the exception (top 4 bits) and
     /// the length of the instruction bytes (lower 4 bits).
@@ -40,6 +40,16 @@ fn slice_to_arr(s: &[u8]) -> [u8; 15] {
     out[..s.len()].copy_from_slice(s);
 
     out
+}
+
+/// A helper function for [Instruction::increment].
+#[derive(Debug, Clone, Copy)]
+pub enum ByteWrapped {
+    /// Incrementing the byte did not wrap.
+    NoWrap,
+
+    /// Incrementing the byte did wrap.
+    Wrap,
 }
 
 impl Instruction {
@@ -76,6 +86,97 @@ impl Instruction {
     pub fn all_bytes(&self) -> &[u8; 15] {
         &self.bytes
     }
+
+    /// Increment an instruction byte by mutating the
+    /// instruction. Return whether the byte actually wrapped to zero.
+    pub fn increment_mut(&mut self, pos: usize) -> ByteWrapped {
+        assert!(pos < self.len());
+
+        let (v, overflow) = self.bytes[pos].overflowing_add(1);
+
+        self.bytes[pos] = v;
+
+        if overflow {
+            ByteWrapped::Wrap
+        } else {
+            ByteWrapped::NoWrap
+        }
+    }
+
+    /// The immutable version of [increment_mut]. This returns a new
+    /// instruction and the wrapping indicator instead of modifying an
+    /// instruction.
+    pub fn increment(&self, pos: usize) -> (Instruction, ByteWrapped) {
+        let mut new = self.clone();
+        let wrapped = new.increment_mut(pos);
+
+        (new, wrapped)
+    }
+}
+
+/// An iterator that interpolates missing lines from Baresifter output.
+#[derive(Debug)]
+struct InterpolateIterator {
+    /// The current instruction. This is the instruction that is
+    /// returned from [next].
+    current: Instruction,
+
+    /// We iterate to this instruction (but not including it).
+    target: Instruction,
+
+    /// The byte position in the instruction byte array that we are
+    /// currently incrementing. This moves from end to start through
+    /// instruction.
+    pos: usize,
+}
+
+impl InterpolateIterator {
+    /// Construct a new instruction iterator that generates all
+    /// instructions between these two.
+    ///
+    /// The target instruction will not be emitted.
+    fn new(from: &Instruction, to: &Instruction) -> InterpolateIterator {
+        assert!(from.len() > 0);
+
+        InterpolateIterator {
+            current: from.clone(),
+            target: to.clone(),
+
+            pos: from.len() - 1,
+        }
+    }
+}
+
+impl Iterator for InterpolateIterator {
+    type Item = Instruction;
+
+    /// Return the next instruction in the sequence.
+    ///
+    /// This uses the same logic as
+    /// `search_engine::find_next_candidate` in the C++ baresifter
+    /// code to generate the same instruction sequences.
+    fn next(&mut self) -> Option<Instruction> {
+        if self.current.all_bytes() == self.target.all_bytes() {
+            None
+        } else {
+            let ret = self.current.clone();
+
+            loop {
+                if let ByteWrapped::Wrap = self.current.increment_mut(self.pos) {
+                    // We wrapped and we are already at the
+                    // beginning. This should not happen.
+                    assert!(self.pos > 0);
+
+                    self.pos -= 1;
+                    continue;
+                }
+
+                break;
+            }
+
+            Some(ret)
+        }
+    }
 }
 
 #[cfg(test)]
@@ -107,5 +208,25 @@ mod tests {
             Instruction::new(0x0e, &[0; 16]),
             Err(InvalidInstruction::default())
         );
+    }
+
+    #[test]
+    fn test_instruction_iterator() -> Result<(), InvalidInstruction> {
+        let instr_start = Instruction::new(0xe, &[0x0d, 0xff, 0xfe])?;
+        let instr_end = Instruction::new(0xe, &[0x0f, 0x00, 0x00])?;
+
+        let inbetween: Vec<Instruction> =
+            InterpolateIterator::new(&instr_start, &instr_end).collect();
+
+        assert_eq!(
+            inbetween,
+            vec![
+                Instruction::new(0xe, &[0x0d, 0xff, 0xfe])?,
+                Instruction::new(0xe, &[0x0d, 0xff, 0xff])?,
+                Instruction::new(0xe, &[0x0e, 0x00, 0x00])?,
+            ]
+        );
+
+        Ok(())
     }
 }
