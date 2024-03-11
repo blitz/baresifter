ffa417c53df4976c3d6586e0d5272b77d126b6b8
superfury <superfury@live.nl>
2024-03-11 20:26:06 +0100

    - There are no REX prefixes on x86.

--- src/common/search.cpp
+++ src/common/search.cpp
@@ -32,10 +32,12 @@ static constexpr int opcode_to_prefix_group(uint8_t byte)
   case 0x67:                    // address size override
     group = 3;
     break;
+#ifndef __x86_64__
   case 0x40 ... 0x4F:           // REX prefixes
     group = 4;
     break;
   }
+#endif
 
   return group;
 }
